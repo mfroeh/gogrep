473a4717a6bf47a628c72b20c32fb67724c741ce
mfroeh <mfroeh0@pm.me>
2025-07-05 13:31:15 +0200

    define nix derivation for gogrep

--- flake.nix
+++ flake.nix
@@ -1,16 +1,29 @@
 {
-	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-	};
-	outputs = {self, nixpkgs, ...}: 
-		let
-			systems = [ "aarch64-darwin" "x86_64-linux" ];
-		in {
-			devShells = nixpkgs.lib.genAttrs systems (system: rec { 
-				pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
-				default = pkgs.mkShell {
-					buildInputs = with pkgs; [ go gopls ];
-				};
-			});
-		};
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "x86_64-linux"
+      ];
+    in
+    {
+      packages = nixpkgs.lib.genAttrs systems (system: rec {
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+        default = pkgs.buildGoModule {
+          name = "gogrep";
+          src = ./.;
+          vendorHash = "sha256-WIzP3PgNYzB13okvLvjKpgEp7dqxUytyquj9HY4vVIg=";
+
+          # this becomes available in the implicitly defined devShell
+          nativeBuildInputs = [ pkgs.gopls ];
+        };
+      });
+    };
 }
